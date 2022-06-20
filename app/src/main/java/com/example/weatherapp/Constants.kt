974b9820314c7b5f2293221a70fa6b16bf2996ae
Osayuki's Work <oethe68@gmail.com>
2022-06-19 22:54:43 -0400

    Finished Project

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.weatherapp
 
 object Constants {
-    //    https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fcfbd110ee65c7150605ea8aceea&units=imperial
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
+    const val API_KEY = "YOUR API KEY"
 }
--- app/src/main/java/com/example/weatherapp/network/Endpoints.kt
+++ app/src/main/java/com/example/weatherapp/network/Endpoints.kt
@@ -1,6 +1,6 @@
 package com.example.weatherapp.network
 
-import retrofit2.http.GET
+
 
 object Endpoints {
     const val DAILY_ENDPOINT = "data/2.5/forecast/daily"
