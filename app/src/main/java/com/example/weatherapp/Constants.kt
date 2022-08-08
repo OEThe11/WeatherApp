c87b8adc59dfc69d04c25816932ab021972bdef2
Osayuki's Work <oethe68@gmail.com>
2022-08-07 20:08:09 -0400

    The Real Complete Project

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -2,5 +2,5 @@ package com.example.weatherapp
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "YOUR_API_KEY"
+    const val API_KEY = "YOUR_API_KEYS"
 }
